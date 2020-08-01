a500462abecbd7daeae2a891e0922ce8edf8c479
Mario Payan <mariopayan@alertlogic.com>
2020-08-01 12:43:59 -0500

    Updates

---
+++ precache-manifest.4ffdb39a528efbbfa813979f14914564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474e003dafae5c1a1fb6f47ea718dfed",
+    "url": "/picoycedula/index.html"
+  },
+  {
+    "revision": "6a475a1691bae7166a61",
+    "url": "/picoycedula/static/css/main.b3659515.chunk.css"
+  },
+  {
+    "revision": "1a1bb6d2824f29ddf663",
+    "url": "/picoycedula/static/js/2.78f4d9a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/picoycedula/static/js/2.78f4d9a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a475a1691bae7166a61",
+    "url": "/picoycedula/static/js/main.903cacd9.chunk.js"
+  },
+  {
+    "revision": "9fdf67ac60c9c4ebc9c5",
+    "url": "/picoycedula/static/js/runtime-main.68981660.js"
+  }
+]);
