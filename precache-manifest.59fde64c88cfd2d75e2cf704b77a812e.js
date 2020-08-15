88791cc140deef3f8da33c7796d9692235665206
Anderson Rubiano <anderrubiano@outlook.com>
2020-08-14 23:56:47 -0500

    Updates

---
+++ precache-manifest.59fde64c88cfd2d75e2cf704b77a812e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0026e3e385c8b07822ddec0e4c2f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "429f9310f46c66c9b30b",
+    "url": "/static/css/main.7e380b0d.chunk.css"
+  },
+  {
+    "revision": "85454ccf8374ba4e5158",
+    "url": "/static/js/2.f0deebd3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f0deebd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429f9310f46c66c9b30b",
+    "url": "/static/js/main.653be0a5.chunk.js"
+  },
+  {
+    "revision": "88ce5d766d2c1e6aee8e",
+    "url": "/static/js/runtime-main.2466efc2.js"
+  }
+]);
