f2092b0e08b021dd1d177bfe74a3f864750f3977
Anderson Rubiano <anderrubiano@outlook.com>
2020-12-21 17:41:43 -0500

    update bogota

--- src/data.tsx
+++ src/data.tsx
@@ -2,9 +2,9 @@ import { Entity } from 'components/index.types'
 
 export type City =
  /* | 'BARRANQUILLA'
-  | 'BOGOTA'
   | 'BUCARAMANGA'
   | 'BUGA'*/
+  | 'BOGOTA'
   | 'CALI'
  /* | 'CUCUTA'
   | 'JAMUNDI'
@@ -28,10 +28,10 @@ export const subjects: { [type in 'issue' | 'contact']: string } = {
 export const cities: { [city in City]: string } = {
  /* ARMENIA: 'Armenia',
   BARRANQUILLA: 'Barranquilla',
-  BOGOTA: 'Bogotá',
   BUCARAMANGA: 'Bucaramanga',
   BUENAVENTURA: 'Buenaventura',
   BUGA: 'Buga',*/
+  BOGOTA: 'Bogotá',
   CALI: 'Cali',
  /* CARTAGENA: 'Cartagena',
   CERRITO: 'El Cerrito',
@@ -46,8 +46,8 @@ export const cities: { [city in City]: string } = {
 
 export const source: { [city: string]: string } = {
   CALI: 'https://twitter.com/AlcaldiaDeCali',
-  /*BOGOTA: 'https://twitter.com/ClaudiaLopez/status/1300387861263458304/photo/1',
-  BARRANQUILLA: 'https://twitter.com/alcaldiabquilla/header_photo',
+  BOGOTA: 'https://twitter.com/ClaudiaLopez/status/1300387861263458304/photo/1',
+  /*BARRANQUILLA: 'https://twitter.com/alcaldiabquilla/header_photo',
   BUGA: 'https://twitter.com/AlcaldiaBuga/status/1289249870298009601',
   CUCUTA: 'https://www.instagram.com/p/CDWNXnPnruH',
   JAMUNDI:
@@ -78,7 +78,34 @@ const pico_y_cedula: { [city: string]: { [date: string]: number[] } } = {
     '20/12/22': [2, 4, 6, 8, 0],
     '20/12/23': [1, 3, 5, 7, 9],
   },
-
+BOGOTA: {
+    '20/12/21': [1, 3, 5, 7, 9],
+    '20/12/22': [2, 4, 6, 8, 0],
+    '20/12/23': [1, 3, 5, 7, 9],
+    '20/12/24': [2, 4, 6, 8, 0],
+    '20/12/25': [1, 3, 5, 7, 9],
+    '20/12/26': [2, 4, 6, 8, 0],
+    '20/12/27': [1, 3, 5, 7, 9],
+    '20/12/28': [2, 4, 6, 8, 0],
+    '20/12/29': [1, 3, 5, 7, 9],
+    '20/12/30': [2, 4, 6, 8, 0],
+    '20/12/31': [1, 3, 5, 7, 9],
+    '20/12/01': [1, 3, 5, 7, 9],
+    '20/12/02': [2, 4, 6, 8, 0],
+    '20/12/03': [1, 3, 5, 7, 9],
+    '20/12/04': [2, 4, 6, 8, 0],
+    '20/12/05': [1, 3, 5, 7, 9],
+    '20/12/06': [2, 4, 6, 8, 0],
+    '20/12/07': [1, 3, 5, 7, 9],
+    '20/12/08': [2, 4, 6, 8, 0],
+    '20/12/09': [1, 3, 5, 7, 9],
+    '20/12/10': [2, 4, 6, 8, 0],
+    '20/12/11': [1, 3, 5, 7, 9],
+    '20/12/12': [2, 4, 6, 8, 0],
+    '20/12/13': [1, 3, 5, 7, 9],
+    '20/12/14': [2, 4, 6, 8, 0],
+    '20/12/15': [1, 3, 5, 7, 9],
+  },
 }
 
 const pico_y_placa: { [city: string]: { [date: string]: number[] } } = {
